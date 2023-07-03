4bd7686ac92a33edb731e0b45b90043dd3920fa2
WojciechBuda <wojciech.buda6@gmail.com>
2023-07-03 22:23:07 +0200

    about-motion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,11 +11,59 @@ const About = () => {
   return (
     <section className="section" id="about" ref={ref}>
       <div className="container mx-auto">
-        <div>
-
-          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top">zdj</div>
-
-          <div>t</div>
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+          ></motion.div>
+          <div className="flex-1">
+            <h5 className="h2 font-t  text-rose-800">About me</h5>
+            <h3 className="h3 mb-4">I`m</h3>
+            <p className=" mb-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+              voluptatem, ex, veritatis soluta sequi hic ipsam consequuntur
+              vitae voluptates veniam maxime recusandae doloremque magnam
+              reiciendis inventore excepturi corrupti, laborum fuga?
+            </p>
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+              <div>
+                <div className="text-[40px] font-teritary text-gradient mb-2">
+                  {InView ? <CountUp start={0} end={12} duration={3} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Months of <br />
+                  Experiance
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-teritary text-gradient mb-2">
+                  {InView ? <CountUp start={0} end={4} duration={3} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Projects <br />
+                  Completed
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-teritary text-gradient mb-2">
+                  {InView ? <CountUp start={0} end={12} duration={3} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Months of <br />
+                  Experiance
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg">Contact me</button>
+              <a href="" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
