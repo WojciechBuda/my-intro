9d17be44b28b75e62be93669ae7c0b496f0918f0
WojciechBuda <wojciech.buda6@gmail.com>
2023-07-27 11:21:57 +0200

    hobby changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import favicon from './favicon.ico';
+import favicon from "./favicon.ico";
 
 const setFavicon = () => {
-  const link = document.createElement('link');
-  link.type = 'image/x-icon';
-  link.rel = 'shortcut icon';
+  const link = document.createElement("link");
+  link.type = "image/x-icon";
+  link.rel = "shortcut icon";
   link.href = favicon;
-  document.getElementsByTagName('head')[0].appendChild(link);
+  document.getElementsByTagName("head")[0].appendChild(link);
 };
 
 setFavicon();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
