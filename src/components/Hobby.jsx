af4517350fc12df8517ac79ce078f2519b6c0e7b
WojciechBuda <wojciech.buda6@gmail.com>
2023-07-28 16:52:29 +0200

    Bottom bg onClick

--- src/components/Hobby.jsx
+++ src/components/Hobby.jsx
@@ -6,22 +6,49 @@ const Hobby = () => {
   const handleAnswerClick = (answer) => {
     console.log(`Wybrana odpowiedź: ${answer}`);
   };
-dupa
+
   return (
-    <section className="section" id="services">
-      <div className="flex flex-row space-x-6 flex-2 mx-auto">
-        <div className="mx-auto ml-24">
-          <h2 className="h2 text-gradient">My Hobby</h2>
-          <h3 className="h3">These are activities <br/>I am a huge fan of</h3>
-        </div>
-        <div className="flex h-auto w-auto justify-items-center lg:flex-row lg:items-center space-x-8">
-          <div className="flex-1">
+    <section
+      className="section min-h-[85vh] lg:min-h[78vh] flex items-center"
+      id="services"
+    >
+      <div className="flex mx-auto shadow-md overflow-hidden">
+        <div className="flex md:flex-row">
+          <div className="p-8 flex flex-col text-center gap-y-8 lg:items-center lg:gap-x-12">
+            <a className="block mt-1 text-lg leading-tight font-extrabold">
+              <motion.h2
+                variants={fadeIn("right", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.7 }}
+              >
+                <h2 className="h2 text-4xl text-gradient top-0 lg:block flex-1 text-center lg:text-left no-select">
+                  My Hobby
+                </h2>
+              </motion.h2>
+            </a>
+            <span className="mt-2 text-2xl font-bold mb-8 mx-2-lg mx-auto lg:mx-0 no-select">
+              <motion.h3
+                variants={fadeIn("up", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.7 }}
+              >
+                <p>
+                  These are activities <br />I am a huge fan of
+                </p>
+              </motion.h3>
+            </span>
+          </div>
+          <div className="flex-1 lg:space-x-6 hidden lg:block">
             <motion.button
               onClick={() => handleAnswerClick("winter")}
               variants={fadeIn("right", 0.3)}
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.3 }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
               style={{
                 backgroundImage: 'url("src/assets/winter.jpg")',
                 backgroundSize: "cover",
@@ -31,14 +58,14 @@ dupa
                 height: "350px",
               }}
             ></motion.button>
-          </div>
-          <div className="flex-1 rounded-lg overflow-hidden">
             <motion.button
               onClick={() => handleAnswerClick("climb")}
               variants={fadeIn("up", 0.3)}
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.3 }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
               style={{
                 backgroundImage: 'url("src/assets/climb.jpg")',
                 backgroundSize: "cover",
@@ -48,14 +75,14 @@ dupa
                 height: "350px",
               }}
             ></motion.button>
-          </div>
-          <div className="flex-1 transition ease-in-out">
             <motion.button
               onClick={() => handleAnswerClick("summit")}
               variants={fadeIn("left", 0.3)}
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.3 }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
               style={{
                 backgroundImage: 'url("src/assets/ice_axe.jpg")',
                 backgroundSize: "cover",
